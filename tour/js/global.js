cb94da644b3db71834817082fad088a4348d43a6
824247231@qq.com <zyq824247231>
2020-06-12 11:52:48 +0800

    url update

---
+++ tour/js/global.js
@@ -0,0 +1,8 @@
+var base_url='http://localhost:8080/s';
+//var base_url='http://www.feelcode.cn/s';
+
+var back_base_url='http://localhost:8082/';
+//var back_base_url='http://www.feelcode:8082/';
+
+var file_upload_url='http://localhost:8081/uploadByMap';
+//var file_upload_url='http://www.feelcode:8081/uploadByMap';
