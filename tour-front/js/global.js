03de3cbf0a930fa2757a6266fac5639a075191cc
824247231@qq.com <zyq824247231>
2020-06-15 14:12:44 +0800

    地址变更，添加mongodb-file-server.jar

--- tour-front/js/global.js
+++ tour-front/js/global.js
@@ -2,4 +2,7 @@ var base_url='http://localhost:8080/c';
 //var base_url='http://www.feelcode.cn:8080/c';
 
 var back_base_url='http://localhost:8082/';
-//var back_base_url='http://www.feelcode:8082/';
+//var back_base_url='http://www.feelcode.cn:8082/';
+
+var file_view_url='http://localhost:8081/';
+//var file_view_url='http://www.feelcode.cn:8081/';
